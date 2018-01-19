477a866dafdd0924090b9f1dd3a902147056dd7e
ssing903 <ssing902@my.centennialcollege.ca>
2018-01-18 22:01:58 -0500

    added array and control

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,23 +4,39 @@
 
 
     "use strict";
+
+    // define an array for html elements
+    var paragraphElements = [];
     // code here
-   var paragraphOne = document.getElementById("paragraphOne");
-   var paragraphTwo = document.getElementById("paragraphTwo");
-   var paragraphThree = document.getElementById("paragraphThree");
+    paragraphElements[0] = document.getElementById("paragraphOne");
+    paragraphElements[1] = document.getElementById("paragraphTwo");
+    paragraphElements[2] = document.getElementById("paragraphThree");
    
+   // define your pargarph array
+   var paragraphs = [];
+
+   // data for my pages
+   paragraphs[0] = "This is my first paragraph.Its visible on first Page";
+   paragraphs[1] = "This is my second paragraph.Its visible on second Page";
+   paragraphs[2] = "This is my third paragraph.Its visible on third Page";
+   // second way to define an array
+  // var paragraphs = new Array();
+
    //check to see i paragrapgh one exits
    if(paragraphOne)
    {
-     console.log("Paragraph One exist")
+     //console.log("Paragraph One exist")
+     paragraphOne.textContent = paragraphs[0];
    }
    if(paragraphTwo)
    {
-      console.log("Paragraph Two exist")
+     // console.log("Paragraph Two exist")
+     paragraphTwo.textContent = paragraphs[1];     
    }
 
    if(paragraphThree)
    {
-      console.log("Paragraph Three exist")
+     //console.log("Paragraph Three exist")
+     paragraphThree.textContent = paragraphs[2];     
    }
 })();
