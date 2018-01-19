cd95fab9f1d7f1a0cfc6945577d48184fb659c6c
ssing903 <ssing902@my.centennialcollege.ca>
2018-01-18 21:37:27 -0500

    added something

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,6 +5,22 @@
 
     "use strict";
     // code here
+   var paragraphOne = document.getElementById("paragraphOne");
+   var paragraphTwo = document.getElementById("paragraphTwo");
+   var paragraphThree = document.getElementById("paragraphThree");
    
-  
+   //check to see i paragrapgh one exits
+   if(paragraphOne)
+   {
+     console.log("Paragraph One exist")
+   }
+   if(paragraphTwo)
+   {
+      console.log("Paragraph Two exist")
+   }
+
+   if(paragraphThree)
+   {
+      console.log("Paragraph Three exist")
+   }
 })();
